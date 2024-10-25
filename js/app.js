bbfdd2eb7b3b1bf5c9a8798eb91fd85f87d9d4e4
Franklin <fran.developer.2022@gmail.com>
2024-10-25 01:01:56 -0600

    API DB Spring

--- js/app.js
+++ js/app.js
@@ -7,9 +7,13 @@ const $sendButton = document.querySelector('#send-button');
 
 const API_KEY = 'api_key'; // Reemplazar con API Key
 const API_KEY_IP = 'api_key_ip'; // Reemplazar con API Key
-const URL_IP = `https://ipinfo.io/?token=${API_KEY_IP}`;
 
-let json_ip = "";
+const URL_IP = `https://ipinfo.io/?token=${API_KEY_IP}`;
+const URL_OpenAI = 'https://api.openai.com/v1/chat/completions'; // POST
+
+const URL_SAVE_DB = 'http://localhost:8080/api/preguntas'; // POST
+const URL_RESPUESTA_DB = 'http://localhost:8080/api/preguntas/respuesta'; // POST
+const URL_EXIST_DB = 'http://localhost:8080/api/preguntas/existe'; // POST
 
 
 // Inicializar la conversación con el rol de médico
@@ -67,8 +71,18 @@ $chatForm.addEventListener('submit', async (event) => {
   // Mostrar el $loader mientras se espera la respuesta
   $loader.style.display = 'block';
 
-  // Consumir la API de OpenAI
-  const response = await sendMessageToChatGPT(userMessage);
+  const apiDbAvailable = await checkApiDbAvailability();
+
+  let response;
+  if (apiDbAvailable) {
+    // Consumir la API de OpenAI Con Guardar
+    response = await sendMessageToChatGPTandSave(userMessage);
+  } else {
+    console.log("La API de la base de datos no está disponible.");
+    // Consumir la API de OpenAI Sin Guardar
+    response = await sendMessageToChatGPT(userMessage);
+  }
+  
 
   // Ocultar el $loader cuando llega la respuesta
   $loader.style.display = 'none';
@@ -77,14 +91,6 @@ $chatForm.addEventListener('submit', async (event) => {
   displayMessageWordByWord(response, 'bot');
   
 });
-
-function displayMessage(message, sender) {
-  const messageElement = document.createElement('div');
-  messageElement.classList.add('message', sender);
-  messageElement.textContent = message;
-  $chatBox.appendChild(messageElement);
-  $chatBox.scrollTop = $chatBox.scrollHeight; // Hacer scroll hasta abajo
-}
 
 function formatResponse(response) {
    // Reemplazar saltos de línea con <br>
@@ -103,6 +109,16 @@ function formatResponse(response) {
    return formatted;
 }
 
+//respuesta instantanea
+function displayMessage(message, sender) {
+  const messageElement = document.createElement('div');
+  messageElement.classList.add('message', sender);
+  messageElement.textContent = message;
+  $chatBox.appendChild(messageElement);
+  $chatBox.scrollTop = $chatBox.scrollHeight; // Hacer scroll hasta abajo
+}
+
+// Simula respuesta escrita
 function displayMessageWordByWord(message, sender) {
   const messageElement = document.createElement('div');
   messageElement.classList.add('message', sender);
@@ -128,13 +144,13 @@ function displayMessageWordByWord(message, sender) {
   displayNextWord(); // Iniciar el ciclo de mostrar palabras
 }
 
-
+//Sin Guardar
 async function sendMessageToChatGPT(userMessage) {
   try {
     // Agregar el mensaje del usuario a la lista de mensajes
     messages.push({ role: 'user', content: userMessage });
 
-    const response = await fetch('https://api.openai.com/v1/chat/completions', {
+    const response = await fetch(URL_OpenAI, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -165,5 +181,107 @@ async function sendMessageToChatGPT(userMessage) {
     $sendButton.disabled = false;
     return 'Oops! Algo salió mal. Inténtalo de nuevo.';
     
+  }
+}
+
+//Con Guardar
+async function sendMessageToChatGPTandSave(userMessage) {
+  try {
+    // Agregar el mensaje del usuario a la lista de mensajes
+    messages.push({ role: 'user', content: userMessage });
+
+    // Consultar API interna para verificar si existe una respuesta guardada
+    const existResponse = await fetch(URL_EXIST_DB, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ pregunta: userMessage }),
+    });
+
+    const existsData = await existResponse.json();
+
+    // Si la respuesta existe en la API interna, obtener la respuesta sin consultar OpenAI
+    if (existsData === true) {
+      const getDbAnswerResponse = await fetch(URL_RESPUESTA_DB, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ pregunta: userMessage }),
+      });
+
+      const answerDbData = await getDbAnswerResponse.json();
+      //console.log("answerDbData", answerDbData);
+      const respuesta = answerDbData.respuesta || 'No se encontró una respuesta guardada';
+
+      messages.push({ role: 'assistant', content: respuesta });
+      return respuesta;
+    }
+
+    const openAIResponse = await fetch(URL_OpenAI, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: 'gpt-4o', // Cambia el modelo si es necesario
+        messages: messages, // Usar la lista de mensajes
+      }),
+    });
+    
+    const dataOpenAi = await openAIResponse.json();
+    //console.log("dataOpenAi", dataOpenAi);
+
+    // Verificar que choices esté presente y no vacío
+    if (dataOpenAi.choices && dataOpenAi.choices.length > 0) {
+      const openAIResponseContent = dataOpenAi.choices[0].message.content;
+      messages.push({ role: 'assistant', content: openAIResponseContent });
+      
+      // Guardar la nueva pregunta y respuesta en la API interna
+      await fetch(URL_SAVE_DB, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          pregunta: userMessage,
+          respuesta: openAIResponseContent,
+        }),
+      });
+
+      return openAIResponseContent;
+    } else {
+      return 'No se recibió una respuesta válida de la API.';
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    //Activar boton
+    $sendButton.disabled = false;
+    return 'Oops! Algo salió mal. Inténtalo de nuevo.';
+    
+  }
+}
+
+async function checkApiDbAvailability() {
+  try {
+    const response = await fetch(URL_EXIST_DB, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ pregunta: 'check_api_availability' }) // Puedes enviar un valor dummy para probar la conexión
+    });
+
+    // Verificamos que la respuesta tenga un status en el rango 200-299
+    if (response.ok) {
+      return true; // La API está disponible
+    } else {
+      return false; // La API respondió pero con algún error
+    }
+  } catch (error) {
+    console.error('Error al verificar la disponibilidad de la API:', error);
+    return false; // Error en la conexión a la API
   }
 }
